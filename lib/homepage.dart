de8ba6109e8b824b3185c457a3f86b6f24c1d7f7
unknown <maryam>
2024-05-26 16:17:59 +0300

    solve problem uploading

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -20,7 +18,6 @@ class homepage extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: Container(
-          
           height: double.infinity,
           width: double.infinity,
           color: blackblue,
@@ -28,83 +25,100 @@ class homepage extends StatelessWidget {
             //scrossAxisAlignment: CrossAxisAlignment.start,
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
-            Align(
-              alignment: Alignment.topLeft,
-              child: Container(
-              width: 427,
-              height: 502,
-              decoration: BoxDecoration(image: DecorationImage(image: AssetImage(ImagesName.osscam))),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Container(
+                  width: 427,
+                  height: 502,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage(ImagesName.osscam))),
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: InkWell(
-                onTap: () {
-                   Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => Addproview()));
-                },
-                child: create_container(text: "Create")),
-            ),
-            
-             
-             InkWell(
-              onTap: () {
-                 showModalBottomSheet(
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => Addproview()));
+                    },
+                    child: create_container(text: "Create")),
+              ),
+              InkWell(
+                  onTap: () {
+                    showModalBottomSheet(
                         context: context,
                         builder: (BuildContext context) {
                           ResultModel response;
                           return Container(
                               decoration: BoxDecoration(
-                                borderRadius: BorderRadius.only(topLeft:Radius.circular(15),topRight:Radius.circular(15), ),
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(15),
+                                  topRight: Radius.circular(15),
+                                ),
                                 color: accent,
                               ),
                               width: double.infinity,
                               height: 360,
                               child: Column(
                                 children: [
-                                  SizedBox(height: 30,),
+                                  SizedBox(
+                                    height: 30,
+                                  ),
                                   Padding(
                                     padding: const EdgeInsets.all(8.0),
                                     child: Row(
-                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
                                       children: [
-                                        Text("Enter project's\n ID ..",style: TextStyle(color:blackblue,fontSize: 22 ),)
-                                     ,Container(
-                                      width: 100,
-                                      height:100 ,
-                                      decoration: BoxDecoration(
-                                        image: DecorationImage(image: AssetImage(ImagesName.tree))
-                                      ),
-                                     )
+                                        Text(
+                                          "Enter project's\n ID ..",
+                                          style: TextStyle(
+                                              color: blackblue, fontSize: 22),
+                                        ),
+                                        Container(
+                                          width: 100,
+                                          height: 100,
+                                          decoration: BoxDecoration(
+                                              image: DecorationImage(
+                                                  image: AssetImage(
+                                                      ImagesName.tree))),
+                                        )
                                       ],
                                     ),
                                   ),
                                   Padding(
                                     padding: const EdgeInsets.all(8.0),
-                                    child: whitTextField(text:id_controller , hieght: 49),
+                                    child: whitTextField(
+                                        text: id_controller, hieght: 49),
                                   ),
-                                  SizedBox(height: 40,),
-                                  
-                                 InkWell(
-                                  onTap: ()async {
-                                         response=  await join_progect(progectmodel(name: "", description: "", status: "", createdBy: "", lastModified: "", id: int.parse(id_controller.text), createDate: ""));
-                                         if (response is MessageModel) {
-                                           
-                                 print("id has sended");
-                                 
-                                         } else {
-                                           
-                                         }
-                                  },
-                                  child: confirm())
+                                  SizedBox(
+                                    height: 40,
+                                  ),
+                                  InkWell(
+                                      onTap: () async {
+                                        response = await join_progect(
+                                            progectmodel(
+                                                name: "",
+                                                description: "",
+                                                status: "",
+                                                createdBy: "",
+                                                lastModified: "",
+                                                id: int.parse(
+                                                    id_controller.text),
+                                                createDate: ""));
+                                        if (response is MessageModel) {
+                                          print("id has sended");
+                                        } else {}
+                                      },
+                                      child: confirm())
                                 ],
                               ));
                         });
-         
-              },
-              child: DottedContainer()),
-             
-           
+                  },
+                  child: DottedContainer()),
             ],
           ),
         ),
